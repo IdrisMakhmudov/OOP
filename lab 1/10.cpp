28a9bc91b8fa6c6dc78101895948474e75491e94
Idris <idris.makhmud8v@mail.ru>
2020-02-11 17:20:48 +0300

    done

--- lab 1/10.cpp
+++ lab 1/10.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
 int main()
 {
+
 int funt, shil, pen;
+
 cin >> funt >> shil >> pen;
-cout << "In outdated system " << funt <<'.' << shil <<'.' << pen << endl;
-cout << "Decimal system " << funt <<'.' << shil*12 + pen;
+
+cout << "In outdated system: " << funt <<'.' << shil <<'.' << pen << endl;
+
+cout << "Decimal system: " << funt <<'.' << shil*12 + pen;
+
 
 return 0;
 }
